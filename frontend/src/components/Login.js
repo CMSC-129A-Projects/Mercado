1e6ddfb6797dec72ef29581074c2b3c24eb0f433
MariaNiela <45968760+MariaNiela@users.noreply.github.com>
2021-03-23 22:13:35 +0800

    ...

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,39 +1,35 @@
-import React from 'react';
-import {
-    Form,
-    FormControl,
-    Button
-} from 'react-bootstrap';
+import React, { Component } from "react";
+import { Container } from "react-bootstrap";
 
-class Login extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+export default class Login extends Component {
+  render() {
+    return (
+      <Container>
+      <form>
+        <h3>Log in</h3>
+        <div className="form-group">
+            <label>Email</label>
+            <input type="email" className="form-control" placeholder="Enter email" />
+        </div>
 
-    render() {
-        return (
-            <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-          
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form> 
-        );
-    }
+        <div className="form-group">
+            <label>Password</label>
+            <input type="password" className="form-control" placeholder="Enter password" />
+        </div>
+
+        <div className="form-group">
+            <div className="custom-control custom-checkbox">
+                <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+            </div>
+        </div>
+
+        <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
+        <p className="forgot-password text-right">
+            Forgot <a href="#">password?</a>
+        </p>
+      </form>
+      </Container>
+    );
+  }
 }
-
-export default Login;
