85124288fda37741229f1d84be85e6b63c94fc61
Gabriel John <luffymanzanares101@gmail.com>
2021-04-08 12:52:49 +0800

    Login component created
    12:30pm-12:52pm

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,74 +1,83 @@
 import React from "react";
-import { Link, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { Container, Form, InputGroup, Button } from "react-bootstrap";
 
-function Login() {
-    const [state, setState] = React.useState({
-        phone_number: '',
-        password: '',
-    });
+export default class Login extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            phone_number: '',
+            password: '',
+            errors: {},
+        };
 
-    function handlePhoneFieldChange(e) {
+        this.handlePhoneField = this.handlePhoneField.bind(this);
+        this.handlePasswordField = this.handlePasswordField.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handlePhoneField(e) {
         const re = /^[0-9\b]+$/;
 
         // If value is not blank, test using regex
         if (e.target.value === '' || re.test(e.target.value)) {
-            setState({ ...state, [e.target.name]: e.target.value })
+            this.setState({ phone_number: e.target.value })
         }
     }
 
-    function handleChange(e) {
-        setState({ ...state, [e.target.name]: e.target.value });
+    handlePasswordField(e) {
+        this.setState({
+            password: e.target.value,
+        });
     }
 
-    function onSubmit(e) {
-        
+    handleSubmit(e) {
+        e.preventDefault();
     }
 
-    return (
-        <Container>
-            <Form onSubmit={onSubmit}>
-                <h3>Log in</h3>
-                <Form.Group>
-                    <Form.Label>Phone</Form.Label>
-                    <InputGroup>
-                        <InputGroup.Prepend>
-                        <InputGroup.Text>+63</InputGroup.Text>
-                        </InputGroup.Prepend>
+    render() {
+        return (
+            <Container className="mt-4">
+                <Form>
+                    <h3>Log in</h3>
+                    <Form.Group>
+                        <Form.Label>Phone</Form.Label>
+                        <InputGroup>
+                            <InputGroup.Prepend>
+                            <InputGroup.Text>+63</InputGroup.Text>
+                            </InputGroup.Prepend>
+                            <Form.Control 
+                                required
+                                maxLength="10"
+                                type="text" 
+                                id="phone_number" 
+                                name="phone_number"
+                                value={this.state.phone_number}
+                                onChange={this.handlePhoneField}
+                                placeholder="Phone"
+                            />
+                        </InputGroup>
+                    </Form.Group>
+    
+                    <Form.Group>
+                        <Form.Label>Password</Form.Label>
                         <Form.Control 
-                            required={true}
-                            maxLength="10"
-                            value={'                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             +63'+state.phone_number}
-                            onChange={handlePhoneFieldChange}
-                            type="text" 
-                            id="phoneNumber" 
-                            name="phone_number"
-                            placeholder="Phone"
+                            required
+                            type="password"
+                            id="password" 
+                            name="password" 
+                            value={this.state.password}
+                            onChange={this.handlePasswordField}
+                            placeholder="Password"
                         />
-                    </InputGroup>
-                </Form.Group>
-
-                <Form.Group>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control 
-                        required={true}
-                        value={state.password}
-                        onChange={handleChange}
-                        type="password"
-                        id="password" 
-                        name="password" 
-                        placeholder="Password"
-                    />
-                </Form.Group>
-
-                <Button variant="primary" type="submit">
-                    Log in
-                </Button>
-                <p>Forgot <Link to="/reset_password">password?</Link></p>
-            </Form>
-        </Container>
-    );
+                    </Form.Group>
+    
+                    <Button variant="primary" type="submit" onClick={this.handleSubmit}>
+                        Log in
+                    </Button>
+                    <p>Forgot <Link to="/reset_password">password?</Link></p>
+                </Form>
+            </Container>
+        );
+    }
 }
-
-export default Login;
