cd0fd145403294dd942420734affdc65935ebb86
MariaNiela <45968760+MariaNiela@users.noreply.github.com>
2021-04-14 21:45:55 +0800

    UI home page update

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -4,6 +4,33 @@ function HomePage() {
     return (
         <React.Fragment>
             <h1>Home Page</h1>
+            <div class="album py-5 bg-light">
+                <div class="container">
+
+                    <div class="pb-3 h5">All Books</div>
+                    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-5 g-3">
+
+                        {% for product in products %}
+
+                        <div class="col">
+                            <div class="card shadow-sm">
+                                <img class="img-fluid" alt="Responsive image" src="{{ product.image.url }}"></img>
+                                <div class="card-body">
+                                    <p class="card-text">
+                                        <a class="text-dark text-decoration-none" href="{{ product.get_absolute_url }}">{{ product_title }}</a>
+                                    </p>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <small class="text-muted">9min read</small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {% endfor %}  
+
+                    </div>
+                </div>
+            </div>
         </React.Fragment>
     );
 }
