e3037d07e17af05937467704d60cf945fb97dc77
Gabriel John <luffymanzanares101@gmail.com>
2021-06-03 11:52:12 +0800

    😴 feat(backend): added `review_count` and `average_rating` in product serializer

--- backend/store/serializers.py
+++ backend/store/serializers.py
@@ -1,4 +1,5 @@
-from rest_framework import fields, serializers
+from rest_framework import serializers
+from django.db.models import Avg
 
 from .models import (
     Category,
@@ -60,12 +61,25 @@ class OrderDetailSerializer(serializers.ModelSerializer):
 class ProductReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductReview
-        fields = '__all__'
+        fields = [
+            'id',
+            'product',
+            'rating',
+            'title',
+            'body',
+            'image',
+            'slug',
+            'created_at',
+            'last_updated',
+        ]
+        read_only_fields = ['user', 'product']
 
 
 class ProductSerializer(serializers.ModelSerializer):
     category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
     product_product_reviews = ProductReviewSerializer(many=True, read_only=True)
+    review_count = serializers.SerializerMethodField()
+    average_rating = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
@@ -86,6 +100,17 @@ class ProductSerializer(serializers.ModelSerializer):
             'is_active',
             'created_at',
             'last_updated',
-            'product_product_reviews'
+            'product_product_reviews',
+            'review_count',
+            'average_rating'
         ]
         read_only_fields = ['user']
+
+    def get_review_count(self, obj):
+        return obj.product_product_reviews.count()
+
+    def get_average_rating(self, obj):
+        average = obj.product_product_reviews.all().aggregate(Avg('rating')).get('rating__avg')
+        if average == None:
+            return 0
+        return average
