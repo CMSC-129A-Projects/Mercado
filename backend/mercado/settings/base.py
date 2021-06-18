6bab5ee1d3061dac04edef9aa5a0128458ac4dc4
Gabriel John <luffymanzanares101@gmail.com>
2021-06-19 00:11:11 +0800

    😴 feat(drf): set default pagination class to limit offset pagination (20 page size)

--- backend/mercado/settings/base.py
+++ backend/mercado/settings/base.py
@@ -90,7 +90,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 50
 }
 
 CORS_ALLOWED_ORIGINS = [
